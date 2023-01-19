77bbc1b3fa2c28184226bef67ff97f2037df602e
LexJoss <ajoss@hotmail.co.uk>
2023-01-19 09:01:30 +0000

    removes errant console logs

--- db/controller.js
+++ db/controller.js
@@ -24,7 +24,6 @@ const getReviews = (req, res, next) => {
 
 const getReviewsByID = (req, res, next) => {
     const query = req.params.review_id
-    console.log(query)
     fetchReviewByID(query)
     .then((reviewsID) => {
         res.status(200).send(reviewsID)
